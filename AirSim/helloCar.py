e12234315566028cd986bdd92f243c6b27db205a
Shreyas Shrawage <shreyas.shrawage2000@gmail.com>
2022-01-01 19:46:11 +0530

    Added pytorch model importing code

--- AirSim/helloCar.py
+++ AirSim/helloCar.py
@@ -4,23 +4,39 @@ import cv2
 import numpy as np
 import keyboard
 
+import torch
+from torch.autograd import Variable
+import torchvision.transforms as transforms
+from model import *
+
 client = airsim.CarClient() # https://microsoft.github.io/AirSim/api_docs/html/_modules/airsim/client.html#CarClient
 client.confirmConnection()
 apiControl = True
 client.enableApiControl(apiControl)
 client.armDisarm(apiControl)
 car_controls = airsim.CarControls()
-desired_speed = 12
+desired_speed = 8
+
+model_path = 'model_pt9.h5'
+checkpoint = torch.load(model_path, map_location=lambda storage, loc: storage)
+model = checkpoint['model']
+transformations = transforms.Compose([transforms.Lambda(lambda x: (x / 255.0) - 0.5)])
+main_steering_angle = 0
 
 while True:
     car_state = client.getCarState()
     print("Speed %d, Gear %d" % (car_state.speed, car_state.gear))
-
-    if car_state.speed < desired_speed - 1:
+    if client.isApiControlEnabled() and car_state.speed < desired_speed - 1:
         car_controls.brake = 0
-        car_controls.steering = 0
         car_controls.throttle = 1
+        car_controls.steering = main_steering_angle
         client.setCarControls(car_controls)
+
+    if keyboard.is_pressed('q'):
+        apiControl = not apiControl
+        client.enableApiControl(apiControl)
+        client.armDisarm(apiControl)
+        time.sleep(1)
 
     '''
     Scene = 0, 
@@ -34,33 +50,36 @@ while True:
     OpticalFlow = 8,
     OpticalFlowVis = 9
     '''
-    responses = client.simGetImages([airsim.ImageRequest(0, airsim.ImageType.Scene, False, False)])
-    for response in responses:
-        if response.pixels_as_float:
-            print("Type %d, size %d" % (response.image_type, len(response.image_data_float)))
-            airsim.write_pfm('py1.pfm', airsim.get_pfm_array(response))
-        else:
-            print("Type %d, size %d" % (response.image_type, len(response.image_data_uint8)))
-            img1d = np.frombuffer(response.image_data_uint8, dtype=np.uint8) #get numpy array
-            img_rgb = img1d.reshape(response.height, response.width, 3) #reshape array to 3 channel image array H X W X 3
-            image = img_rgb[65:-25, :, :]
-            image = cv2.cvtColor(image, cv2.COLOR_BGR2YUV)
-            image = cv2.resize(image, (200, 66), cv2.INTER_AREA)
-            cv2.imwrite('numpy.png', image)
 
-    if (car_state.speed > desired_speed):
-        car_controls.throttle = 0
-        car_controls.brake = 1
-        client.setCarControls(car_controls)
+    if client.isApiControlEnabled():
+        responses = client.simGetImages([airsim.ImageRequest(0, airsim.ImageType.Scene, False, False)])
+        for response in responses:
+            if response.pixels_as_float:
+                print("Type %d, size %d" % (response.image_type, len(response.image_data_float)))
+                airsim.write_pfm('py1.pfm', airsim.get_pfm_array(response))
+            else:
+                print("Type %d, size %d" % (response.image_type, len(response.image_data_uint8)))
+                img1d = np.frombuffer(response.image_data_uint8, dtype=np.uint8) #get numpy array
+                img_rgb = img1d.reshape(response.height, response.width, 3) #reshape array to 3 channel image array H X W X 3
+                image = img_rgb[65:-25, :, :]
+                image = cv2.cvtColor(image, cv2.COLOR_BGR2YUV)
+                image = cv2.resize(image, (320, 70), cv2.INTER_AREA)
+                image = transformations(image)
+                image = torch.Tensor(image)
+                image = image.view(1, 3, 70, 320)
+                image = Variable(image)
+                main_steering_angle = model(image).view(-1).data.numpy()[0]
+                main_steering_angle = float(main_steering_angle)
+                print(f"st angle = {main_steering_angle}")
 
-    if (client.simGetCollisionInfo()).has_collided:
-        client.reset()
-        apiControl = True
-        client.enableApiControl(apiControl)
-        client.armDisarm(True)
+        if client.isApiControlEnabled() and (car_state.speed > desired_speed):
+            car_controls.throttle = 0
+            car_controls.brake = 1
+            client.setCarControls(car_controls)
 
-    if keyboard.is_pressed('q'):
-        apiControl = not apiControl
-        client.enableApiControl(apiControl)
-        client.armDisarm(apiControl)
+        if (client.simGetCollisionInfo()).has_collided:
+            client.reset()
+            apiControl = True
+            client.enableApiControl(apiControl)
+            client.armDisarm(True)
 
